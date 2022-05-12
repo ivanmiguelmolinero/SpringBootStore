12acf9e0e26298761c43e673316ef599f34a4a16
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-05-12 11:56:37 +0200

    Service creado

---
+++ src/main/java/practica/vermont/store/product/service/ProductServiceImpl.java
@@ -0,0 +1,72 @@
+package practica.vermont.store.product.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import practica.vermont.store.product.entity.Category;
+import practica.vermont.store.product.entity.Product;
+import practica.vermont.store.product.repository.ProductRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> listAllProduct() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        product.setStatus("CREATED");
+        product.setCreateAt(new Date());
+
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        Product productDB = getProduct(product.getId());
+        if (null == productDB) {
+            return null;
+        }
+        productDB.setName(product.getName());
+        productDB.setDescription(product.getDescription());
+        productDB.setCategory(product.getCategory());
+        productDB.setPrice(product.getPrice());
+        return productRepository.save(productDB);
+    }
+
+    @Override
+    public Product deleteProduct(Long id) {
+        Product productDB = getProduct(id);
+        if (null == productDB) {
+            return null;
+        }
+        productDB.setStatus("DELETED");
+        return productRepository.save(productDB);
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return productRepository.findByCategory(category);
+    }
+
+    @Override
+    public Product updateStock(Long id, Double quantity) {
+        Product productDB = getProduct(id);
+        if (null == productDB) {
+            return null;
+        }
+        Double stock = productDB.getStock() + quantity;
+        productDB.setStock(stock);
+        return productRepository.save(productDB);
+    }
+}
