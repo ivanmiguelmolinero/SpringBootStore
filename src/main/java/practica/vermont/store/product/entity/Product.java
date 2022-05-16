c2e88e0e073bf6d8fd92a2916f6e7e3f37ed7dbe
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-05-16 13:31:20 +0200

    Primera API terminada

--- src/main/java/practica/vermont/store/product/entity/Product.java
+++ src/main/java/practica/vermont/store/product/entity/Product.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -19,8 +22,10 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+   @NotEmpty(message = "El nombre no debe estar vacío")
     private String name;
     private String description;
+    @Positive(message = "El stock debe ser mayor que cero")
     private Double stock;
     private Double price;
     private String status;
@@ -29,6 +34,7 @@ public class Product {
     @Temporal(TemporalType.TIMESTAMP) // Fecha y hora
     private Date createAt;
 
+    @NotNull(message = "La categoría no puede estar vacía")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
